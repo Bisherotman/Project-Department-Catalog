d688dc5b9402fe5e81faca3168557227bb19cc25
Bisherotman <ba.othman@baytalebaa.com>
2025-09-14 14:28:52 +0300

    Update subcategory.js

--- subcategory.js
+++ subcategory.js
@@ -96,7 +96,9 @@ searchFab?.addEventListener("click", ()=> searchBox?.focus());
 async function loadProducts(){
   // We match the field 'path' that stores the exact label like "WC - Wall-Hung"
   const qRef = query(collection(db, "products"),
-                   where("path", "==", PATH));
+  where("path", "==", PATH));
+  orderBy("createdAt", "desc")
+
   const snap = await getDocs(qRef);
   products = snap.docs.map(d => ({ id:d.id, ...d.data() }));
   filtered = [...products];
