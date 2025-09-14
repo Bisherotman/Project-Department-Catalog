c1b679468845f03f2a7c56086e4928c6eb8e4345
Bisherotman <ba.othman@baytalebaa.com>
2025-09-14 18:44:34 +0300

    Update categories-data.js

--- categories-data.js
+++ categories-data.js
@@ -20,12 +20,119 @@ export const categories = [
       "Wall-hung"
     ]
   },
-  // ... باقي التصنيفات
+  {
+    name: "Urinal",
+    subcategories: [
+      "Top inlet",
+      "Rear inlet"
+    ]
+  },
+  {
+    name: "Wash Basins",
+    subcategories: [
+      "Wall-mount & Pedestals",
+      "One piece",
+      "Floor-standing",
+      "Over counter",
+      "Under counter",
+      "Top counter",
+      "Cabinet"
+    ]
+  },
+  {
+    name: "Showers and Bathtubs",
+    subcategories: [
+      "Free-standing",
+      "Drop-in",
+      "Hydromassage",
+      "Shower tray",
+      "Shower cabinet"
+    ]
+  },
+  {
+    name: "Faucets",
+    subcategories: [
+      "Basin Mixers",
+      "Bath & Shower Mixers",
+      "Shower systems",
+      "Shower Jets",
+      "Kitchen Mixers",
+      "Bidet Mixers",
+      "Ablution Mixers",
+      "Taps"
+    ]
+  },
+  {
+    name: "Fittings",
+    subcategories: [
+      "Angle Valves",
+      "Showers",
+      "Flexible hoses",
+      "Traps",
+      "Strainers",
+      "Pop-ups"
+    ]
+  },
+  {
+    name: "Sinks",
+    subcategories: [
+      "Stainless steel bowls",
+      "Cast iron bowls",
+      "Vitreous China",
+      "Service"
+    ]
+  },
+  {
+    name: "Drains",
+    subcategories: [
+      "Floor drains",
+      "Clean-outs",
+      "Shower Channels"
+    ]
+  },
+  {
+    name: "Cisterns",
+    subcategories: [
+      "Concealed for WCs",
+      "Concealed for Urinals",
+      "Exposed",
+      "Elements",
+      "Flush Valves",
+      "Actuator plates"
+    ]
+  },
+  {
+    name: "Accessories",
+    subcategories: [
+      "Sets",
+      "Singles",
+      "Electronic",
+      "Mirrors",
+      "Handicapped"
+    ]
+  },
+  {
+    name: "Water Heater",
+    subcategories: [
+      "Wall-hung",
+      "Central",
+      "Solar"
+    ]
+  }
 ];
 
 // 🏷️ قائمة البراندات الثابتة
 export const brands = [
   "Grohe",
   "Kohler",
-  "Nofer"
+  "Nofer",
+  "Silver",
+  "Ideal Standard",
+  "Geberit",
+  "Laufen",
+  "Saudi Ceramic",
+  "Ferplast",
+  "Rodi",
+  "Gedy",
+  "Ariston"
 ];
